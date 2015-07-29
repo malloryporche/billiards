0dd79b9f6c980f7b2aebe4ce47ca2c7549bb7027
Mallory Porche <malloryburke@gmail.com>
2015-07-29 13:48:31 -0700

    Project reset

--- client/views/teams.js
+++ client/views/teams.js
@@ -1,5 +1,35 @@
 Template.teams.helpers({
+	isCreatingTeam: function(){
+    	return Session.get('isCreatingTeam');
+  	},
 	teams: function(){
 		return Teams.find();
+	}
+});
+
+Template.teams.events({
+	'click a.create': function(e, t){
+		e.preventDefault();
+
+		Session.set('isCreatingTeam', true);
+	},
+	'click a.cancel': function(e, t){
+		e.preventDefault();
+
+		Session.set('isCreatingTeam', false);
+	},
+	'submit form.create-team': function(e,t){
+		e.preventDefault();
+
+		var teamName = t.$('input[name=name]').val();
+		Teams.insert({name: teamName}, function(error, _id){
+			if(error){
+				alert(error);
+				Session.set('isCreatingTeam', true);
+				Tracker.afterFlush(function(){
+					t.$('input[name=name]').val(teamName);
+				});
+			};
+		});
 	}
 });
