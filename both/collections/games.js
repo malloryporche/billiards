1941afed59a900d7479e605c698e2d9a8b894958
Mallory Porche <malloryburke@gmail.com>
2015-07-31 11:26:39 -0700

    Finished permissions with teams, games, players

--- both/collections/games.js
+++ both/collections/games.js
@@ -12,3 +12,40 @@ Games.allow({
   },
   fetch: ['ownerId']
 });
+
+Meteor.methods({
+	gamesInsert: function(teamOneId, teamTwoId) {
+		var teamOne = Teams.findOne({_id: teamOneId});
+		var teamTwo = Teams.findOne({_id: teamTwoId});
+
+		var teamOneData = {
+			_id: teamOne._id,
+			name: teamOne.name,
+			score: 0
+		};
+
+		var teamTwoData = {
+			_id: teamTwo._id,
+			name: teamTwo.name,
+			score: 0
+		};
+
+		var game = {
+			ownerId: Meteor.userId(),
+			createdAt: new Date(),
+			teams: [teamOneData, teamTwoData],
+			completed: false
+		};
+
+		var gameId = Games.insert(game);
+
+
+		//llUpdate each team's cached array of game ids
+		Teams.update({_id: teamOneData._id}, {$addToSet: { gameIds: gameId}});
+		Teams.update({_id: teamTwoData._id}, {$addToSet: { gameIds: gameId}});
+
+		//Copy Meteor.insert(), which just returns the _id
+		return gameId;
+	}
+
+});
